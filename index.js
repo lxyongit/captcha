ba826d4fa8c6951e3a61e84a111b5e204a8a811e
lxyong <1171114775@qq.com>
2021-03-06 03:05:47 +0800

    feat: 新建

---
+++ index.js
@@ -0,0 +1,40 @@
+const ffi = require('ffi-napi')
+const path = require('path')
+const ref = require('ref-napi')
+const ArrayType = require('ref-array-napi')
+const { readFile, readFileSync } = require('fs')
+const { resolve } = require('path')
+const ocrDllPath = path.resolve(__dirname, 'ocr.dll')
+const byteArray = ArrayType(ref.types.byte)
+const ocrDll = ffi.Library(ocrDllPath, {
+    init: ['void', []],
+    ocr: ['string', [byteArray, 'int']]
+})
+
+// 是否已初始化
+let isCalled = false
+/**
+ * @param {Buffer | string} 图片的buffer数据或图片路径
+ * @return string 识别到的二维码
+ * */ 
+ function codeOcr(imgBuffer) {
+    if(!isCalled) {
+        ocrDll.init()
+        isCalled = true
+    }
+    return new Promise((resolve, reject) => {
+        if(typeof imgBuffer === 'string') {
+            readFile(imgBuffer, (err, data) => {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve(ocrDll.ocr(data, data.length))
+                }
+            })
+        } else {
+            resolve(ocrDll.ocr(imgBuffer, imgBuffer.length))
+        }
+    })
+}
+
+module.exports = codeOcr
